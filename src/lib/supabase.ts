03a17ac06201b81ab196ccb5c008755474f4bcaa
Uster <smpl.qa.manthan@gmail.com>
2025-11-25 21:56:09 +0530

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '../types/database';
-const supabaseUrl = 'https://shpmwwyrsrvvbbqaqkgw.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNocG13d3lyc3J2dmJicWFxa2d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4OTA5NjAsImV4cCI6MjA3OTQ2Njk2MH0.IE0cqS6HIlLqJ7LXRU01QTK8rA8GuLwM4AeEwrU2OdU';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
